97188f124aa7b8b34ec3acf56c598e05098329a3
Emil Dabrowski <kingemil115@gmail.com>
2020-10-05 20:13:54 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace CanSatGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.txtLat = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLong = new System.Windows.Forms.TextBox();
+            this.MapSearch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.map = new GMap.NET.WindowsForms.GMapControl();
+            this.SuspendLayout();
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(328, 450);
+            this.splitter1.TabIndex = 0;
+            this.splitter1.TabStop = false;
+            // 
+            // txtLat
+            // 
+            this.txtLat.Location = new System.Drawing.Point(491, 81);
+            this.txtLat.Name = "txtLat";
+            this.txtLat.Size = new System.Drawing.Size(100, 20);
+            this.txtLat.TabIndex = 1;
+            this.txtLat.TextChanged += new System.EventHandler(this.txtLat_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(488, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Longitude";
+            // 
+            // txtLong
+            // 
+            this.txtLong.Location = new System.Drawing.Point(491, 129);
+            this.txtLong.Name = "txtLong";
+            this.txtLong.Size = new System.Drawing.Size(100, 20);
+            this.txtLong.TabIndex = 3;
+            this.txtLong.TextChanged += new System.EventHandler(this.txtLong_TextChanged);
+            // 
+            // MapSearch
+            // 
+            this.MapSearch.Location = new System.Drawing.Point(505, 173);
+            this.MapSearch.Name = "MapSearch";
+            this.MapSearch.Size = new System.Drawing.Size(75, 23);
+            this.MapSearch.TabIndex = 5;
+            this.MapSearch.Text = "Find Me";
+            this.MapSearch.UseVisualStyleBackColor = true;
+            this.MapSearch.Click += new System.EventHandler(this.MapSearch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(488, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Latitude";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // map
+            // 
+            this.map.Bearing = 0F;
+            this.map.CanDragMap = true;
+            this.map.EmptyTileColor = System.Drawing.Color.Navy;
+            this.map.GrayScaleMode = false;
+            this.map.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.map.LevelsKeepInMemmory = 5;
+            this.map.Location = new System.Drawing.Point(0, 0);
+            this.map.MarkersEnabled = true;
+            this.map.MaxZoom = 2;
+            this.map.MinZoom = 2;
+            this.map.MouseWheelZoomEnabled = true;
+            this.map.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.map.Name = "map";
+            this.map.NegativeMode = false;
+            this.map.PolygonsEnabled = true;
+            this.map.RetryLoadTile = 0;
+            this.map.RoutesEnabled = true;
+            this.map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.map.ShowTileGridLines = false;
+            this.map.Size = new System.Drawing.Size(328, 450);
+            this.map.TabIndex = 7;
+            this.map.Zoom = 0D;
+            this.map.Load += new System.EventHandler(this.map_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.map);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MapSearch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLong);
+            this.Controls.Add(this.txtLat);
+            this.Controls.Add(this.splitter1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.TextBox txtLat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLong;
+        private System.Windows.Forms.Button MapSearch;
+        private System.Windows.Forms.Label label1;
+        private GMap.NET.WindowsForms.GMapControl map;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+
+namespace CanSatGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void MapSearch_Click(object sender, EventArgs e)
+        {
+            GMapProviders.GoogleMap.ApiKey = @"AIzaSyAZouhXULQgPGPckADOmiHqfCc_YvD5QzQ";
+            map.DragButton = MouseButtons.Left;
+            map.MapProvider = GMapProviders.GoogleMap;
+            double lat = Convert.ToDouble(txtLat.Text);
+            double longt = Convert.ToDouble(txtLong.Text);
+            map.Position = new PointLatLng(lat, longt);
+            map.MinZoom = 0;
+            map.MaxZoom = 25;
+            map.Zoom = 10;
+        }
+
+        private void txtLat_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtLong_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void map_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
