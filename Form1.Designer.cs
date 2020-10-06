7e88050d35ff78d31edc3b745460f35fec098d23
Emil Dabrowski <kingemil115@gmail.com>
2020-10-06 19:26:19 +0200

    dodano text box data stream i spardowano do odpowiednich text boxow

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,26 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.components = new System.ComponentModel.Container();
             this.txtLat = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.txtLong = new System.Windows.Forms.TextBox();
             this.MapSearch = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.map = new GMap.NET.WindowsForms.GMapControl();
+            this.SerialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.DataStream = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tmptxt = new System.Windows.Forms.TextBox();
+            this.psrtxt = new System.Windows.Forms.TextBox();
+            this.hghttxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // splitter1
-            // 
-            this.splitter1.Location = new System.Drawing.Point(0, 0);
-            this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(328, 450);
-            this.splitter1.TabIndex = 0;
-            this.splitter1.TabStop = false;
             // 
             // txtLat
             // 
-            this.txtLat.Location = new System.Drawing.Point(491, 81);
+            this.txtLat.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLat.Location = new System.Drawing.Point(233, 27);
             this.txtLat.Name = "txtLat";
             this.txtLat.Size = new System.Drawing.Size(100, 20);
             this.txtLat.TabIndex = 1;
@@ -55,8 +57,9 @@
             // 
             // label2
             // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(488, 113);
+            this.label2.Location = new System.Drawing.Point(230, 59);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(54, 13);
             this.label2.TabIndex = 4;
@@ -64,7 +67,8 @@
             // 
             // txtLong
             // 
-            this.txtLong.Location = new System.Drawing.Point(491, 129);
+            this.txtLong.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLong.Location = new System.Drawing.Point(233, 75);
             this.txtLong.Name = "txtLong";
             this.txtLong.Size = new System.Drawing.Size(100, 20);
             this.txtLong.TabIndex = 3;
@@ -72,7 +76,8 @@
             // 
             // MapSearch
             // 
-            this.MapSearch.Location = new System.Drawing.Point(505, 173);
+            this.MapSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.MapSearch.Location = new System.Drawing.Point(247, 119);
             this.MapSearch.Name = "MapSearch";
             this.MapSearch.Size = new System.Drawing.Size(75, 23);
             this.MapSearch.TabIndex = 5;
@@ -82,8 +87,9 @@
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(488, 65);
+            this.label1.Location = new System.Drawing.Point(230, 11);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(45, 13);
             this.label1.TabIndex = 6;
@@ -93,6 +99,7 @@
             // map
             // 
             this.map.Bearing = 0F;
+            this.map.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.map.CanDragMap = true;
             this.map.EmptyTileColor = System.Drawing.Color.Navy;
             this.map.GrayScaleMode = false;
@@ -112,23 +119,101 @@
             this.map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
             this.map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
             this.map.ShowTileGridLines = false;
-            this.map.Size = new System.Drawing.Size(328, 450);
+            this.map.Size = new System.Drawing.Size(227, 236);
             this.map.TabIndex = 7;
             this.map.Zoom = 0D;
             this.map.Load += new System.EventHandler(this.map_Load);
+            // 
+            // SerialPort1
+            // 
+            this.SerialPort1.PortName = "COM3";
+            // 
+            // DataStream
+            // 
+            this.DataStream.Location = new System.Drawing.Point(369, 27);
+            this.DataStream.Name = "DataStream";
+            this.DataStream.Size = new System.Drawing.Size(291, 68);
+            this.DataStream.TabIndex = 8;
+            this.DataStream.Text = "";
+            this.DataStream.TextChanged += new System.EventHandler(this.DataStream_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(484, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Data Stream";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // tmptxt
+            // 
+            this.tmptxt.Location = new System.Drawing.Point(781, 23);
+            this.tmptxt.Name = "tmptxt";
+            this.tmptxt.Size = new System.Drawing.Size(100, 20);
+            this.tmptxt.TabIndex = 10;
+            // 
+            // psrtxt
+            // 
+            this.psrtxt.Location = new System.Drawing.Point(781, 75);
+            this.psrtxt.Name = "psrtxt";
+            this.psrtxt.Size = new System.Drawing.Size(100, 20);
+            this.psrtxt.TabIndex = 11;
+            // 
+            // hghttxt
+            // 
+            this.hghttxt.Location = new System.Drawing.Point(781, 49);
+            this.hghttxt.Name = "hghttxt";
+            this.hghttxt.Size = new System.Drawing.Size(100, 20);
+            this.hghttxt.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(707, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Temperature";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(707, 49);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Height";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(707, 81);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Pressure";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1107, 648);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.hghttxt);
+            this.Controls.Add(this.psrtxt);
+            this.Controls.Add(this.tmptxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.DataStream);
             this.Controls.Add(this.map);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.MapSearch);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtLong);
             this.Controls.Add(this.txtLat);
-            this.Controls.Add(this.splitter1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -138,14 +223,21 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Splitter splitter1;
         private System.Windows.Forms.TextBox txtLat;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtLong;
         private System.Windows.Forms.Button MapSearch;
         private System.Windows.Forms.Label label1;
         private GMap.NET.WindowsForms.GMapControl map;
+        private System.IO.Ports.SerialPort SerialPort1;
+        private System.Windows.Forms.RichTextBox DataStream;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tmptxt;
+        private System.Windows.Forms.TextBox psrtxt;
+        private System.Windows.Forms.TextBox hghttxt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,28 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.IO.Ports;
 using GMap.NET; 
 using GMap.NET.MapProviders;
 using GMap.NET.WindowsForms;
-//pianino
+using System.Threading;
 
-//dzialajaca mapa v1.0
+
+
 namespace CanSatGUI
 {
     public partial class Form1 : Form
     {
+        string rxString;
         public Form1()
         {
             InitializeComponent();
+            SerialPort1 = new SerialPort();
+            SerialPort1.PortName = "COM3";
+            SerialPort1.BaudRate = 9600;
+            SerialPort1.Parity = Parity.None;
+            SerialPort1.DataBits = 8;
+            SerialPort1.StopBits = StopBits.One;
+            SerialPort1.Open();
+            SerialPort1.DataReceived += myPort_DataReceived;
         }
-
+                    
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            
         }
 
+        public void UpdateTextboxes(string packet)
+        {
+            string[] packetElems = Utils.ParsePacket(packet); // możesz odp
+            
+            psrtxt.Text = packetElems[1];
+            tmptxt.Text = packetElems[2];
+        }
 
+        //dzialajaca mapa v1.0
         private void MapSearch_Click(object sender, EventArgs e)
         {
             GMapProviders.GoogleMap.ApiKey = @"AIzaSyAZouhXULQgPGPckADOmiHqfCc_YvD5QzQ";
@@ -59,7 +77,57 @@ namespace CanSatGUI
 
         private void label1_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboxserial_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DataStream_TextChanged(object sender, EventArgs e)
+        {
             
+        } // poczatek czesci wykonawczej serial port txt box v1.0
+        private void myPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            rxString = SerialPort1.ReadExisting();
+            this.Invoke(new EventHandler(DisplayText));
+        }
+
+        private void DisplayText(object sender, EventArgs e)
+        {
+            DataStream.AppendText(rxString);
+            UpdateTextboxes(rxString);
+        }
+        //koniec czesci wyckonawczej serial port 
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    class Utils
+    {
+        public static string[] ParsePacket(string packet)
+        {
+            // "$$ZSM-Sat,997.27,1018,END$$$"
+            string[] list = packet.Split(',');
+            return list;
         }
     }
 }
+
+
+
+
